7cf0ca67aa3e14d042e6bd85c40e53f3b5d5dc91
WingLim <643089849@qq.com>
2020-03-18 16:25:16 +0800

    feat: chage parm position

--- HDUCoursesAPI/db_json.py
+++ HDUCoursesAPI/db_json.py
@@ -2,7 +2,7 @@ import json
 
 class DBJson():
 
-    def write(self, data, filename='output'):
+    def write(self, filename='output', data=None):
         s = json.dumps(data, indent = 4, ensure_ascii=False)
         with open(filename + '.json', 'w', encoding = 'utf-8') as f:
             f.write(s)
