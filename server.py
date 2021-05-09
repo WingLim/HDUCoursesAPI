340a0f16f9ec1a28d58083e8e2f4723abeb20e32
WingLim <643089849@qq.com>
2021-05-09 13:44:44 +0800

    feat: replace flask to fastapi

--- server.py
+++ server.py
@@ -1,79 +1,72 @@
-from flask import Flask, jsonify, make_response, request, redirect
+from typing import Optional
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
 from HDUCoursesAPI.db_sqlite import DBSqlite
 from HDUCoursesAPI.db_json import DBJson
-from HDUCoursesAPI.utils import db2dict, count2dict
-from HDUCoursesAPI.course_spider import SpiderThread
-import random
-import time
+from HDUCoursesAPI.utils import db2dict
 
+app = FastAPI()
 db = DBSqlite()
 tb = 'course2020-20212'
-app = Flask(__name__)
 
-@app.route('/courses')
-def index():
-    return redirect('https://winglim.github.io/HDUCoursesAPI/')
 
-@app.route('/courses/query')
-def query():
-    filters = request.args.to_dict()
-    print(filters)
-    tmp = request.args.get('limit')
-    limit = (tmp if tmp != None else 10)
+class Query(BaseModel):
+    status: Optional[int] = None
+    title: Optional[str] = None
+    credit: Optional[int] = None
+    method: Optional[str] = None
+    property: Optional[str] = None
+    teacher: Optional[str] = None
+    class_id: Optional[str] = None
+    time: Optional[str] = None
+    location: Optional[str] = None
+    academic: Optional[str] = None
+    other: Optional[str] = None
+
+
+@app.get("/courses/query")
+def query(
+        req: Request,
+        status: Optional[int] = None,
+        title: Optional[str] = None,
+        credit: Optional[int] = None,
+        method: Optional[str] = None,
+        property: Optional[str] = None,
+        teacher: Optional[str] = None,
+        class_id: Optional[str] = None,
+        time: Optional[str] = None,
+        location: Optional[str] = None,
+        academic: Optional[str] = None,
+        other: Optional[str] = None,
+        limit: int = 10
+):
+    filters = Query(**req.query_params).dict(exclude_unset=True)
     r = db.fetch(tb, filters, limit=limit)
     d = db2dict(r)
     result = DBJson().cook_course_json(d)
-    return make_response(jsonify(result))
+    return result
 
-@app.route('/courses/title/<data>')
-@app.route('/courses/property/<data>')
-@app.route('/courses/teacher/<data>')
-@app.route('/courses/time/<data>')
-def onecolume(data):
-    filters = request.args.to_dict()
-    column = request.path.split('/')[2]
-    tmp = request.args.get('limit')
-    limit = (tmp if tmp != None else 10)
+
+@app.get('/courses/{column}/{data}')
+def one_column(
+        req: Request,
+        column: str,
+        data: str,
+        limit: int = 10,
+        status: Optional[int] = None,
+        title: Optional[str] = None,
+        credit: Optional[int] = None,
+        method: Optional[str] = None,
+        property: Optional[str] = None,
+        teacher: Optional[str] = None,
+        class_id: Optional[str] = None,
+        time: Optional[str] = None,
+        location: Optional[str] = None,
+        academic: Optional[str] = None,
+        other: Optional[str] = None,
+):
+    filters = Query(**req.query_params).dict(exclude_unset=True)
     r = db.fetch(tb, filters, column, data, limit)
-    result = db2dict(r)
-    return make_response(jsonify(result))
-
-@app.route('/courses/teachers')
-def teachers():
-    r = db.fetch_count(tb, 'TEACHER')
-    result = count2dict(r)
-    return make_response(jsonify(result))
-
-exporting_threads = {}
-@app.route('/spider')
-def spider():
-    global exporting_threads
-    year = request.args.get('year')
-    term = request.args.get('term')
-    if year == None or term == None:
-        return make_response(jsonify({
-            'status': 'failed',
-            'msg': 'please input year and term'
-        }))
-    thread_id = random.randint(0, 10000)
-    exporting_threads[thread_id] = SpiderThread(year, term)
-    exporting_threads[thread_id].start()
-    return make_response(jsonify({
-        'status': 'running',
-        'thread_id': thread_id
-    }))
-
-
-@app.route('/status/<int:thread_id>')
-def spider_status(thread_id):
-    global exporting_threads
-    if exporting_threads[thread_id].spider.end_time == 0:
-        end_time = time.time()
-    return make_response(jsonify({
-        'status': exporting_threads[thread_id].spider.status,
-        'page_count': exporting_threads[thread_id].spider.page_count,
-        'total_time': end_time - exporting_threads[thread_id].spider.start_time
-    }))
-
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=3000)
+    d = db2dict(r)
+    result = DBJson().cook_course_json(d)
+    return result
