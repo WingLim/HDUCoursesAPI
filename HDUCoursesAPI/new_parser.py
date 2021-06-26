934bf54b0b6cdb2230e853cbc7ef606b1d3df103
WingLim <winglim@qq.com>
2021-06-26 20:35:14 +0800

    fix: re search

--- HDUCoursesAPI/new_parser.py
+++ HDUCoursesAPI/new_parser.py
@@ -18,7 +18,7 @@ def parse_week(week_info: str) -> dict:
         flag = 1
     if '双' in week_info:
         flag = 2
-    week_info = re.search(r'\d-.\d', week_info).group()
+    week_info = re.search(r'\d-\d\d?', week_info).group()
     start, end = week_info.split("-")
     result = {
         "start": start,
